637091f260ec34eaeb6a9e0e7036f47230aa2804
EidmantasDev <eidmantas.slenys@gmail.com>
2024-04-01 21:22:10 +0300

    Refactored AvailablePlaces component Moved fetching logic to http.js Added navigator.geolocation to get user location Used sortPlacesByDistance from loc.js to sortPlaces by user coordinates and then set available places

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
 import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailabePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -13,22 +15,26 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch('http://localhost:3000/places');
-        const resData = await response.json();
+        const places = await fetchAvailabePlaces();
 
-        if (!response.ok) {
-          throw new Error('Failed to fetch places');
-        }
-
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition(
+          ({ coords: { latitude, longitude } }) => {
+            const sortedPlaces = sortPlacesByDistance(
+              places,
+              latitude,
+              longitude
+            );
+            setAvailablePlaces(sortedPlaces);
+            setIsFetching(false);
+          }
+        );
       } catch (error) {
         setError({
           message:
             error.message || 'Could not fetch places, please try again later.',
         });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();
